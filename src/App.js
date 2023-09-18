217ee2f684b7005936a1f882cf0517062d5dc002
nhanhthat <84766117+nhanhthat@users.noreply.github.com>
2023-09-19 00:12:37 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [link, setLink] = useState()
+  const [linkBackup, setLinkBackup] = useState('')
   const [isComplete, setIsComplete] = useState(false)
   const [isRun, setIsRun] = useState(false)
   const [reload, setReload] = useState(false)
+  const [delete1, setDelete1s] = useState(false)
   useEffect(() => {
-    setTimeout(() => {
+    let int = setTimeout (() => {
       axios.get('http://localhost:5555/get-length')
-      .then (res => {
-        if (res.data.number != 0) {
-          setIsRun(!isRun)
-        } else {
-          setReload(!reload)
-        }
-      })
+        .then (res => {
+          if (res.data.number != 0) {
+            console.log('duc')
+            setIsRun(!isRun)
+          } else {
+            console.log('duc dep trai')
+            setReload(!reload)
+          }
+        })
     }, 500)
     
   }, [reload])
@@ -32,49 +36,66 @@ function App() {
               if (link.queue < num) {
                   num = link.queue
               }
-          })  
+          })
           res.data.forEach((link, index) => {
             if (link.queue == num) {
+              setLinkBackup(link)
               setLink(res.data[index])
             }
           })
         }
-        
       })
   }, [isComplete, isRun])
 
   useEffect(() => {
-    if (link != undefined) {
-      console.log(link)
-      axios.post('http://localhost:5555/api/live_x', {link : link.content})
-        .then (res => {
-          let result = res.data.success
-          if (result == true) {
-            axios.post('http://localhost:5555/update-status', {queue : link.queue, status : 'success'})
-              .then(() => {
-                axios.get('http://localhost:5555/get-link-by-queue?queue='+ link.queue)
-                  .then(res => {
-                    if (res.data.length != 1) {
-                      setReload(!reload)
-                      window.location.reload()
-                    }
-                  })
-              })
-          } else {
-            axios.post('http://localhost:5555/update-status', {queue : link.queue, status : 'fail'})
-              .then(() => {
-                axios.get('http://localhost:5555/get-link-by-queue?queue='+ link.queue)
-                  .then(res => {
-                    if (res.data.length != 1) {
-                      setReload(!reload)
-                      window.location.reload()
-                    }
-                  })
-              })
+    setTimeout(() => {
+      if (link != undefined ) {
+        axios.post('http://localhost:5555/api/live_x', {link : link.content})
+          .then (res => {
+            let result = res.data.success
+            if (result == true) {
+              axios.post('http://localhost:5555/update-status', {queue : link.queue, status : 'success'})
+                .then(() => {
+                  setTimeout(() => {
+                        setDelete1s(!delete1)
+                  }, 2000)
+                })
+            } else {
+              axios.post('http://localhost:5555/update-status', {queue : link.queue, status : 'fail'})
+                .then(() => {
+                  setTimeout(() => {
+                        setDelete1s(!delete1)
+                  }, 2000)
+                })
+            }
+          })
+      }
+    }, 300)
+  }, [link])
+
+  useEffect( () => {
+    if (link != undefined ) {
+      let haha = setInterval(() => {
+        axios.get('http://localhost:5555/get-link-by-queue?queue='+ link.queue)
+        .then(res => {
+          console.log(res.data)
+          if(res.data.status == 200){
+            if (res.data.delete == true) {
+              setTimeout(() => {
+                axios.post('http://localhost:5555/delete', {queue : link.queue})
+                .then(res => {
+                  setReload(!reload)
+                  window.location.reload()
+                  clearInterval(haha)
+                })
+              }, 30000)
+            }
           }
         })
+      }, 100)
     }
-  }, [link])
+
+  }, [delete1])
 
   return (
     <div className="App">
@@ -83,3 +104,4 @@ function App() {
 }
 
 export default App;
+
